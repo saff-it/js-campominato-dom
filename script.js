0e87f948e0a1e82a81d1df96adcf293d6153e506
saff-it <saff_it@ymail.com>
2022-06-24 11:37:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const playButton = document.querySelector('.play-btn');
+const squareContainer = document.querySelector('.square-container');
+
+
+playButton.addEventListener('click', function(){
+    
+    squareContainer.innerHTML = "";
+
+    for ( let i = 0; i < 100; i++) {
+    
+        squareAdding = SquareAdd();
+        squareContainer.append(squareAdding);
+        
+        squareAdding.innerHTML = i;
+    }
+
+});
+
+
+function SquareAdd(){
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.addEventListener('click', function(){
+    
+        newSquare.classList.toggle('skyblue');
+        console.log("hai cliccato il tasto [1]");
+    
+    });
+
+    return newSquare;
+}
